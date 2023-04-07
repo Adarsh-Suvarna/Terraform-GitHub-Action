c298e79d1f4ae318cca3e44995abd7d897bd1ca1
Adarsh Suvarna <amsuvarna27@gmail.com>
2023-04-07 11:39:19 +0530

    Update

--- tfe.tf
+++ tfe.tf
@@ -1,10 +1,20 @@
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
   cloud {
-    hostname = "terraform.adarshasuvarna.in"
+    hostname     = "terraform.adarshasuvarna.in"
     organization = "Niveus-Solutions-Pvt-Ltd"
 
     workspaces {
       name = "Terraform-GitHub-Action"
     }
   }
+}
+
+provider "google" {
+  region = "asia-south1"
 }
